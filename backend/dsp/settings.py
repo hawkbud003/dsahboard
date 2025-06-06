110ad8c694b431fc599fdec640f758db88d5070f
Abhishek <abhishekaryz@gmail.com>
2025-06-06 21:08:20 +0530

    mysql swift

--- backend/dsp/settings.py
+++ backend/dsp/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = "dsp.wsgi.application"
 # Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_DATABASE', default='django'),
+        'USER': env('DB_USERNAME', default='root'),
+        'PASSWORD': env('DB_PASSWORD', default=''),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
